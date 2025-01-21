c5fb581969b3f9d594ce20baa3040872c00444f1
rafsan3051 <rafsan3051@gmail.com>
2025-01-21 19:30:25 +0600

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "34320b519338c717"; }
+/******/ 		__webpack_require__.h = function() { return "b46de9d214860ab9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
