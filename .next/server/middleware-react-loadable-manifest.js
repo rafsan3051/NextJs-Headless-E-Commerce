514efeed9a8b934661cfce9d11542e86e5b4ca55
rafsan3051 <rafsan3051@gmail.com>
2025-01-21 19:20:24 +0600

    commit

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"..\\\\node_modules\\\\@wix\\\\sdk\\\\build\\\\auth\\\\AppStrategy.js -> jose\":{\"id\":\"..\\\\node_modules\\\\@wix\\\\sdk\\\\build\\\\auth\\\\AppStrategy.js -> jose\",\"files\":[]}}"
